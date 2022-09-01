cbbe534b9739d603a7e648fa8ca0934d17ae0eb0
gaigeshen <gaigeshen@qq.com>
2022-09-02 00:35:37 +0800

    去除不需要的代码

--- src/main/java/work/gaigeshen/spring/security/demo/security/AuthenticationTokenMissingException.java
+++ src/main/java/work/gaigeshen/spring/security/demo/security/AuthenticationTokenMissingException.java
@@ -7,10 +7,6 @@ import org.springframework.security.core.AuthenticationException;
  */
 public class AuthenticationTokenMissingException extends AuthenticationException {
 
-    public AuthenticationTokenMissingException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
-
     public AuthenticationTokenMissingException(String msg) {
         super(msg);
     }
