d655b3947c476c859a45fc5eb3a9395507612463
gaigeshen <gaigeshen@qq.com>
2022-09-24 15:22:40 +0800

    代码优化

--- src/main/java/work/gaigeshen/spring/security/demo/commons/web/BusinessErrorResults.java
+++ src/main/java/work/gaigeshen/spring/security/demo/commons/web/BusinessErrorResults.java
@@ -2,6 +2,8 @@ package work.gaigeshen.spring.security.demo.commons.web;
 
 import work.gaigeshen.spring.security.demo.commons.exception.BusinessErrorException;
 
+import static work.gaigeshen.spring.security.demo.commons.web.BusinessErrorResultCode.BUSINESS_ERROR;
+
 /**
  *
  * @author gaigeshen
@@ -11,6 +13,6 @@ public abstract class BusinessErrorResults {
     private BusinessErrorResults() { }
 
     public static Result<?> createResult(BusinessErrorException ex) {
-        return Results.create(DefaultResultCode.create(BusinessErrorResultCode.BUSINESS_ERROR, ex.getMessage()), ex.getData());
+        return Results.create(DefaultResultCode.create(BUSINESS_ERROR, ex.getMessage()), ex.getData());
     }
 }
